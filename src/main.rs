7a668733861deadfa4894b9a31017e1ef322daea
norbytus <norbyt93@gmail.com>
2017-03-22 02:25:14 +0300

    Show answers

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ fn main() {
     let request = translate
         .set_apikey("trnsl.1.1.20170312T094041Z.4da8d12c2c6c961e.4bd73640b569f7bfb32b545e188ea1d79dd9cd0e")
         .translate_from_to(vec!["Hello", "World"], "en-ru");
-    println!("{:?}", request);
 
-   //request.get_text();
+    request.get_text();
 
 }
