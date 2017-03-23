ed75326013af582c97b32aee3dc0ea3d2fb5670c
norbytus <norbyt93@gmail.com>
2017-03-24 01:31:02 +0300

    First work prototype app

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,49 @@ use std::env;
 
 fn main() {
 
-    read_env();
+    let lang = read_env(("-l", "--lang"));
+
+    let text = read_env(("-t", "--text"));
+
+    if !lang.is_some() || !text.is_some() {
+        panic!("Not language or text parameters");
+    }
 
     let translate = YandexTranslate::new();
 
     let request = translate
         .set_apikey("trnsl.1.1.20170312T094041Z.4da8d12c2c6c961e.4bd73640b569f7bfb32b545e188ea1d79dd9cd0e")
-        .translate_from_to(vec!["Hello", "World"], "en-ru");
+        .translate_from_to(vec![&text.unwrap()], &lang.unwrap());
 
-    request.get_text();
+    let mut result_text: String = String::new();
+
+    match request.get_text() {
+        Ok(text) => {
+            result_text = text.iter()
+                .fold(String::new(), |mut s, v| {
+                    s.push_str(v);
+                    s
+                });
+        },
+        Err(e) => panic!("{}", e)
+    }
+
+    println!("{:?}", result_text);
 
 }
 
-fn read_env() {
+fn read_env(args: (&str, &str)) -> Option<String>{
 
     let mut args_iter = env::args().into_iter();
 
-    let lang = args_iter.find( |arg| arg == "-l" || arg == "--lang" );
+    let param = args_iter.find( |arg| arg == args.0 || arg == args.1 );
 
-    let mut lang_param: Option<String> = None;
+    let mut param_value: Option<String> = None;
 
-    if lang.is_some() {
-        lang_param = args_iter.next();
+    if param.is_some() {
+        param_value = args_iter.next();
     }
+
+    param_value
 
 }
