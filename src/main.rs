0b8166d762dde66acb36e5619bd15b4e54608af7
norbytus <norbyt93@gmail.com>
2017-03-27 02:07:04 +0300

    Fix namespave

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 extern crate yandex_translate;
 
-use yandex_translate::yclient::YandexTranslate;
+use yandex_translate::yandex_translate::client::YandexTranslate;
 
 use std::env;
+use std::io::Read;
+use std::fs::{File, OpenOptions};
 
 fn main() {
 
-    let lang = read_env(("-l", "--lang"));
-
-    let text = read_env(("-t", "--text"));
-
-    if !lang.is_some() || !text.is_some() {
-        panic!("Not language or text parameters");
-    }
-
-    let translate = YandexTranslate::new();
+    /*let translate = YandexTranslate::new();
 
     let request = translate
         .set_apikey("trnsl.1.1.20170312T094041Z.4da8d12c2c6c961e.4bd73640b569f7bfb32b545e188ea1d79dd9cd0e")
@@ -33,22 +27,26 @@ fn main() {
         Err(e) => panic!("{}", e)
     }
 
-    println!("{:?}", result_text);
+    println!("{:?}", result_text);*/
 
 }
 
-fn read_env(args: (&str, &str)) -> Option<String>{
+fn read_env(args: (&str, &str), req: bool) -> Option<String> {
 
     let mut args_iter = env::args().into_iter();
 
     let param = args_iter.find( |arg| arg == args.0 || arg == args.1 );
 
-    let mut param_value: Option<String> = None;
-
-    if param.is_some() {
-        param_value = args_iter.next();
+    let mut result: Option<String> = None;
+ 
+    if param.is_none() && req {
+        panic!("Need params {} or {}", args.0, args.1);
+    } else if param.is_none() && !req {
+        result = None;
+    } else {
+        result = args_iter.next();
     }
 
-    param_value
+    result
 
 }
