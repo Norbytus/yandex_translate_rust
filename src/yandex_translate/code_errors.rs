03242a63f69586744e498d56dc677a561b8b8821
norbytus <norbyt93@gmail.com>
2017-04-03 01:17:43 +0300

    Now enum don't have propery and fmt::Display return text from const cariables

--- src/yandex_translate/code_errors.rs
+++ src/yandex_translate/code_errors.rs
@@ -1,14 +1,24 @@
 use std::fmt;
 
+const SUCCESS: &str = "Success.";
+const INVALID_KEY: &str = "Invalid API key.";
+const BLOCKED_KEY: &str = "Blocked API key.";
+const DAILY_LIMIT: &str = "Exceeded the daily limit on the amount of translated text.";
+const MAXIMUM_TEXT_SIZE: &str = "Exceeded the maximum text size.";
+const CANNOT_TRANSLATE: &str = "The text cannot be translated.";
+const NOT_SUPPORTED_LANG: &str = "The specified translation direction is not supported.";
+const JSON_WRONG_FORMAT: &str = "Json wrong format.";
+const ERROR: &str = "Unknown error.";
+
 #[derive(Debug)]
 pub enum Code {
     Success,
-    InvalidKey { code: &'static str },
-    BlockedKey { code: &'static str },
-    DailyLimit { code: &'static str },
-    MaximumTextSize { code: &'static str },
-    CannotTranslate { code: &'static str },
-    NotSupportedLang { code: &'static str },
+    InvalidKey,
+    BlockedKey,
+    DailyLimit,
+    MaximumTextSize,
+    CannotTranslate,
+    NotSupportedLang,
     JsonWrongFormat,
     Error,
 }
@@ -18,15 +28,15 @@ impl fmt::Display for Code {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 
         let printable = match *self {
-            Code::Success => "Success",
-            Code::InvalidKey{ code } => code,
-            Code::BlockedKey{ code } => code,
-            Code::DailyLimit{ code } => code,
-            Code::MaximumTextSize{ code } => code,
-            Code::CannotTranslate{ code } => code,
-            Code::NotSupportedLang{ code } => code,
-            Code::JsonWrongFormat => "JsonWrongFormat",
-            Code::Error => "Error",
+            Code::Success => SUCCESS,
+            Code::InvalidKey => INVALID_KEY,
+            Code::BlockedKey => BLOCKED_KEY,
+            Code::DailyLimit => DAILY_LIMIT,
+            Code::MaximumTextSize => MAXIMUM_TEXT_SIZE,
+            Code::CannotTranslate => CANNOT_TRANSLATE,
+            Code::NotSupportedLang => NOT_SUPPORTED_LANG,
+            Code::JsonWrongFormat => JSON_WRONG_FORMAT,
+            Code::Error => ERROR,
         };
 
         write!(f, "{}", printable)
