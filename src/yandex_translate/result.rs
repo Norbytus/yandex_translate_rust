e90c55358ca2277b3e98c8f31f2a10794413a16c
norbytus <norbyt93@gmail.com>
2017-04-03 01:18:16 +0300

    Add rust doc blocks

--- src/yandex_translate/result.rs
+++ src/yandex_translate/result.rs
@@ -4,6 +4,7 @@ use self::json::JsonValue;
 
 use yandex_translate::code_errors::Code;
 
+///Struct with answer from server in Json format
 #[derive(Debug, Clone)]
 pub struct YandexTranslateResult {
     data: JsonValue,
@@ -11,14 +12,33 @@ pub struct YandexTranslateResult {
 
 impl YandexTranslateResult {
 
+    ///Constructor
+    /// # Example
+    /// ```rust
+    /// use yandex_translate::yandex_translate::client::YandexTranslate;
+    ///
+    /// let request = YandexTranslate::new();
+    /// let result = request.set_apikey("Some_key").translate_from_to(vec!["Hello"], "en-ru");
+    ///
+    /// ```
     pub fn new(json: JsonValue) -> YandexTranslateResult {
         YandexTranslateResult { data: json }
     }
 
-    pub fn get_code(&self) -> Result<Code, Code> {
+    /// Show code answer from server
+    /// # Example
+    /// ```rust
+    /// use yandex_translate::yandex_translate::client::YandexTranslate;
+    ///
+    /// let request = YandexTranslate::new();
+    /// let result = request.set_apikey("Some_code").translate_from_to(vec!["Hello"], "en-ru");
+    /// result.get_code();
+    ///
+    /// ```
+    pub fn get_code(&self) -> Code {
 
         let data = self.data.clone();
-        
+
         if data.has_key("code") {
 
             let (_, code, _) = data["code"]
@@ -27,30 +47,40 @@ impl YandexTranslateResult {
                 .as_parts();
 
             match code {
-                200 => { Ok( Code::Success ) },
-                401 => { Err( Code::InvalidKey{ code: "Invalid API key." } ) },
-                402 => { Err( Code::BlockedKey{ code: "Blocked API key." } ) },
-                404 => { Err( Code::DailyLimit{ code: "Exceeded the daily limit on the amount of translated text." } ) },
-                413 => { Err( Code::MaximumTextSize{ code: "Exceeded the maximum text size." } ) },
-                422 => { Err(Code::CannotTranslate{ code: "The text cannot be translated." } )},
-                501 => { Err(Code::NotSupportedLang{ code: "The specified translation direction is not supported." } )},
-                _ => { Err(Code::Error) },
+                200 => Code::Success,
+                401 => Code::InvalidKey,
+                402 => Code::BlockedKey,
+                404 => Code::DailyLimit,
+                413 => Code::MaximumTextSize,
+                422 => Code::CannotTranslate,
+                501 => Code::NotSupportedLang,
+                _ => Code::Error,
             }
 
         } else {
 
-            Err(Code::JsonWrongFormat)
+            Code::JsonWrongFormat
 
         }
 
     }
 
+    /// Get text answer from server if code answer Success(200)
+    /// # Example
+    /// ```rust
+    /// use yandex_translate::yandex_translate::client::YandexTranslate;
+    ///
+    /// let request = YandexTranslate::new();
+    /// let result = request.set_apikey("Some_code").translate_from_to(vec!["Hello"], "en-ru");
+    /// result.get_text();
+    ///
+    /// ```
     pub fn get_text(&self) -> Result<Vec<String>, Code> {
 
         let code = self.get_code();
 
         match code {
-            Ok(_) => {
+            Code::Success => {
 
                 let data = self.data.clone();
 
@@ -71,9 +101,9 @@ impl YandexTranslateResult {
                 Ok(result_vec)
 
             },
-            Err(e) => {
+            _ => {
 
-                Err(e)
+                Err(code)
 
             }
         }
