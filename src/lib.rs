b72f4daece0a172f9948b7a08f1d7a0232e816a7
Norbytus <norbyt93@gmail.com>
2019-11-30 11:28:03 +0300

    Clean and update crates

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-
-#[path = "yandex_translate/client.rs"]
-pub mod client;
-
-#[path = "yandex_translate/answer.rs"]
-pub mod answer;
